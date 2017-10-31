31789f0858742ab85a13ef31ce9ca94b8e2c68a2
Saul Tello <saultellod@gmail.com>
2017-10-30 22:32:10 -0500

    se optimizo el envio de correoooooooooooooo ooooo

--- polymer-web/src/main/saul/com/pe/CorreoControlle.java
+++ polymer-web/src/main/saul/com/pe/CorreoControlle.java
@@ -8,8 +8,9 @@
  * ---------------------------------------------------------------------------------------------------------------
 * Modificaciones
 * Motivo                      Fecha             Nombre                         Descripción
-* REQ001                    13/07/2016         Julca Amaro Richard Abraham      Se agregaron campos de fecha inicio y fecha final que corresponden a los datos seleccionados en el calendar de la vista 
+* REQ01                    13/07/2016         Julca Amaro Richard Abraham      Se agregaron campos de fecha inicio y fecha final que corresponden a los datos seleccionados en el calendar de la vista 
 *                                                                                reporteEstadoActual.xhtml asimismo con su respectiva validacion
+* REQ02
 * ---------------------------------------------------------------------------------------------------------------
 */
 package pe.com.gmd.sisrec.web.controller;
